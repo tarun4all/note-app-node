26cabf6373047e543899b10955a9e5e37303e9c6
Tarun Bansal <officialtarunbansal@gmail.com>
2020-11-07 15:42:39 +0530

    node app

---
+++ app.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+if (!process.env.MONGO_URL) {
+    console.error("Mongo URL not found. Please create dot env file beafore procedding...");
+}
+
+const mongoose = require("mongoose");
+
+//connect Database from atlas
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/samcrit", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    console.log(err);
+    if (err) throw err;
+    console.log("DB COnnected...");
+});
+
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const { join } = require("path");
+
+//body parser applied as middleware or json pot request
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// const cors = require(`cors`);
+// app.use(cors());
+app.use("*", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    console.log(req.query);
+    next();
+});
+//accessing routes and grouping
+require('./routesGroup');
+app.use('/', require("./routesList"));
+// error handler
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.send({ error: err.message || err });
+});
+
+app.listen(3002, () => {
+    console.log("server starts");
+})
